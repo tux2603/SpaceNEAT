6b3dab877a98b92d472c59192e505cd81e784108
Owen O'Connor <oconnor.57@gmail.com>
2020-06-14 17:27:28 -0400

    Added colorization to help differentiate species

--- Alien.py
+++ Alien.py
@@ -5,7 +5,7 @@ class Alien:
     def __init__(self, shipTexture, shieldTexture, alienBatch, shipGroup, shieldGroup, x=0, y=0):
         self.ship = Sprite(shipTexture, x=x, y=y, blend_src=770, blend_dest=771, batch=alienBatch, group=shipGroup, usage='dynamic', subpixel=False)
         self.shield = Sprite(shieldTexture, x=x, y=y, blend_src=770, blend_dest=771, batch=alienBatch, group=shieldGroup, usage='dynamic', subpixel=False)
-
+        
         # Set the physical aspects of the Alien ship
         self.acceleration = np.array((0, 0), dtype=np.float32)
         self.velocity = np.array((0, 0), dtype=np.float32)
@@ -26,7 +26,7 @@ class Alien:
         self.acceleration = np.array(acceleration, dtype=np.float32)
 
 
-    def update(self, dt, cameraOffset):
+    def update(self, dt):
         """ Moves and rotates the alien space ship thingy
 
         Args:
@@ -39,10 +39,12 @@ class Alien:
         self.angularVelocity += self.angularAcceleration * dt
         self.angularPosition += self.angularVelocity * dt
 
-        self.ship.position = self.position - cameraOffset
         self.ship.rotation = self.angularPosition
-        accelLen = np.linalg.norm(self.acceleration)
+
+    def setCameraPosition(self, cameraOffset):
+        self.ship.position = self.position - cameraOffset
         self.shield.position = self.getShieldCenter() - cameraOffset
+        
         
 
 class Swarm:
@@ -52,6 +54,33 @@ class Swarm:
         for i in range(size):
             self.members.append(Alien(shipTexture, shieldTexture, alienBatch, shipGroup, shieldGroup, x=x, y=y))
 
-    def update(self, dt, cameraOffset):
-        for i in self.members:
-            i.update(dt, cameraOffset)
+    def __iter__(self):
+        return SwarmIterator(self)
+
+    def __len__(self):
+        return len(self.members)
+
+    def update(self, dt):
+        for i in self:
+            i.update(dt)
+
+    def setCameraPosition(self, cameraOffset):
+        for i in self:
+            i.setCameraPosition(cameraOffset)
+
+class SwarmIterator:
+    """ A utility class for the sole and express purpose of iterating over 
+        alien ships within a swarm. It's just a standard copy-pasta style
+        iterator. Nothing fancy
+    """
+
+    def __init__(self, swarm):
+        self._swarm = swarm
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._swarm.members):
+            alien = self._swarm.members[self._index]
+            self._index += 1
+            return alien
+        raise StopIteration
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from math import sin, cos
+from math import sin, cos, pi
 import numpy as np
 import pyglet
 from pyglet.sprite import Sprite
 from pyglet.window import key
+from random import randint
 from time import time
 
 from Alien import Alien, Swarm
@@ -12,16 +13,14 @@ gameWindow = pyglet.window.Window(800, 600, fullscreen=False)
 
 def loadResources():
     global alienShipTexture, alienShieldTexture
-    global fpsDisplay
 
     # Set the locations to find resources
     pyglet.resource.path = ['resources']
     pyglet.resource.reindex()
 
     # Load the alien ship textures
-    alienShipTexture = pyglet.resource.image(
-        'images/alienThingSingleFrame.png')
-    alienShieldTexture = pyglet.resource.image('images/alienShield.png')
+    alienShipTexture = pyglet.resource.image('images/alienShipBW.png')
+    alienShieldTexture = pyglet.resource.image('images/alienShieldBW.png')
 
     # Set the anchors for rotation
     alienShipTexture.anchor_x = alienShipTexture.width / 2
@@ -29,9 +28,49 @@ def loadResources():
     alienShieldTexture.anchor_x = alienShieldTexture.width / 2
     alienShieldTexture.anchor_y = alienShieldTexture.height / 2
 
-    # Create the FPS monitor
-    fpsDisplay = pyglet.window.FPSDisplay(window=gameWindow)
+    print(vars(alienShieldTexture))
+    print(vars(alienShipTexture))
 
+def colorShip(texture, color, colorFade=(0,0,0), colorHighlight=(255,255,255)):
+    global cachedTextures
+
+    if (texture.__hash__(), color, colorFade, colorHighlight) in cachedTextures.keys():
+        print(cachedTextures[(texture.__hash__(), color, colorFade, colorHighlight)])
+        return cachedTextures[(texture.__hash__(), color, colorFade, colorHighlight)]
+
+    else:
+        textureData = texture.get_image_data().get_data('RGBA', texture.width * 4)
+        newTextureData = b''
+        baseColorValue = 72
+
+        fadeDelta = (colorFade[0] - color[0], colorFade[1] - color[1], colorFade[2] - color[2])
+        highlightDelta = (colorHighlight[0] - color[0], colorHighlight[1] - color[1], colorHighlight[2] - color[2])
+
+        # Colorize the alien ship
+        for i in range(0, len(textureData), 4):
+            pixelData = textureData[i:i+4]
+
+            value = pixelData[0]
+
+            # If the value is less than the base color value, we want to fade to black
+            if value < baseColorValue:
+                r = int(color[0] + fadeDelta[0] * (baseColorValue - value) / baseColorValue)
+                g = int(color[1] + fadeDelta[1] * (baseColorValue - value) / baseColorValue)
+                b = int(color[2] + fadeDelta[2] * (baseColorValue - value) / baseColorValue)
+
+            else:
+                r = int(color[0] + highlightDelta[0] * (value - baseColorValue) / (255 - baseColorValue))
+                g = int(color[1] + highlightDelta[1] * (value - baseColorValue) / (255 - baseColorValue))
+                b = int(color[2] + highlightDelta[2] * (value - baseColorValue) / (255 - baseColorValue))
+
+            newTextureData += bytes([r, g, b, pixelData[3]])
+
+        newTexture = pyglet.image.ImageData(texture.width, texture.height, 'RGBA', newTextureData)
+        newTexture.anchor_x = int(texture.anchor_x)
+        newTexture.anchor_y = int(texture.anchor_y)
+
+        cachedTextures[(texture.__hash__(), color, colorFade, colorHighlight)] = newTexture
+        return newTexture
 
 def update(dt):
     global cameraOffset
@@ -41,8 +80,32 @@ def update(dt):
     else:
         alien.setAcceleartion((0,0))
 
-    alien.update(dt, cameraOffset)
+    alien.update(dt)
+    swarm0.update(dt)
+    for i in swarm1:
+        i.update(dt)
+
+    centerCamera(alien.position)
+
+    alien.setCameraPosition(cameraOffset)
+    swarm0.setCameraPosition(cameraOffset)
+    
+    for i in swarm1:
+        i.setCameraPosition(cameraOffset)
+
     pass
+
+def centerCamera(location):
+    """ This function centers the visible portion of the screen around
+        A certain location
+
+
+    Args:
+        location (tuple): The location to be centered
+    """
+    global cameraOffset
+    cameraOffset[0] = location[0] - screenWidth / 2
+    cameraOffset[1] = location[1] - screenHeight / 2
 
 @gameWindow.event
 def on_mouse_press(x, y, button, modifiers):
@@ -66,12 +129,17 @@ def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
 
 @gameWindow.event
 def on_draw():
+
     gameWindow.clear()
     alienBatch.draw()
     fpsDisplay.draw()
 
 
 if __name__ == '__main__':
+    alienShipTexture = None
+    alienShieldTexture = None
+    cachedTextures = {}
+
     # Make the windows fullscreen on the first screen found
     display = pyglet.canvas.get_display()
     screens = display.get_screens()
@@ -87,8 +155,8 @@ if __name__ == '__main__':
     # Keep track of mouse and keyboard states
     mouseLocation = (0, 0)
     mouseState = {pyglet.window.mouse.LEFT: False,
-                  pyglet.window.mouse.MIDDLE: False,
-                  pyglet.window.mouse.RIGHT: False}
+                    pyglet.window.mouse.MIDDLE: False,
+                    pyglet.window.mouse.RIGHT: False}
 
     keyboard = key.KeyStateHandler()
     oldKeyboard = key.KeyStateHandler()
@@ -98,14 +166,37 @@ if __name__ == '__main__':
     loadResources()
 
     alienBatch = pyglet.graphics.Batch()
+
     shipGroup = pyglet.graphics.OrderedGroup(10)
     shieldGroup = pyglet.graphics.OrderedGroup(20)
 
-    alien = Alien(alienShipTexture, alienShieldTexture,
-                  alienBatch, shipGroup, shieldGroup, x=screenWidth / 2, y=screenHeight/2)
+    alien = Alien(alienShipTexture, alienShieldTexture, alienBatch, shipGroup, shieldGroup, x=screenWidth / 2, y=screenHeight/2)
 
-    swarm0 = Swarm(5, alienShipTexture, alienShieldTexture,
-                  alienBatch, shipGroup, shieldGroup, x=screenWidth / 2, y=screenHeight/2)
+    swarmShip = colorShip(alienShipTexture, (255, 0, 255), (0, 0, 255), (255, 0, 0))
+    swarmShield = colorShip(alienShieldTexture, (255, 0, 255), (0, 0, 255), (255, 0, 0))
+    swarm0 = Swarm(0, swarmShip, swarmShield, alienBatch, shipGroup, shieldGroup, x=screenWidth / 2, y=screenHeight/2)
+
+    swarm1 = []
+
+    for i in range(32):
+        c0 = (randint(0, 255), randint(0, 255), randint(0, 255))
+        c1 = (randint(0, 255), randint(0, 255), randint(0, 255))
+        c2 = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+        shipTex = colorShip(alienShipTexture, c0, c1, c2)
+        shieldTex = colorShip(alienShieldTexture, c0, c1, c2)
+        swarm1.append(Alien(shipTex, shieldTex, alienBatch, shipGroup, shieldGroup, x=screenWidth / 2, y=screenHeight/2))
+    # Create the FPS monitor
+    fpsDisplay = pyglet.window.FPSDisplay(window=gameWindow)
+    
+
+    for i, ship in enumerate(swarm0):
+        theta = i * 2 * pi / len(swarm0)
+        ship.setAcceleartion((sin(theta), cos(theta)))
+
+    for i, ship in enumerate(swarm1):
+        theta = (i + 0.5) * 2 * pi / len(swarm1)
+        ship.setAcceleartion((sin(theta), cos(theta)))
 
     pyglet.clock.schedule_interval(update, 1/60.0)
 
